2d16a37ad244f9b2b6cc65bc11c364d24e875a9d
ikuniyoshi-ui <i.kuniyoshi@frogs-corp.jp>
2026-01-30 14:56:28 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,18 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      // 修正: リポジトリ名に合わせて設定
-      base: '/norke-vj/', 
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        // ビルド時に環境変数を注入します
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+export default defineConfig({
+  // リポジトリ名に合わせて設定
+  base: '/norke-vj/', 
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    }
+  }
 });
